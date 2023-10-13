00faa60955c2473f5313418bc93ddc9761742c95
Valerii <badday182@gmail.com>
2023-10-13 16:46:45 +0300

    setSelectedOption(eventKey.value)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Button  from 'react-bootstrap/Button';
+// import CustomDropdown from '../src/components/customDropdown';
+import CustomDropdown from '../src/components/customDropdownTest';
 
 function App() {
   return (
+    
     <div>
-      
+      <p>№ Дослідження</p>
+      <p>Дата</p>
+      <p>П.І.Б</p>
+      <p>Рік народження</p>
+      <CustomDropdown name="Зона дослідження"/>
+      <CustomDropdown name="Проекція"/>
+      <CustomDropdown name="ЄЄД"/>
     </div>
+   
   );
 }
 
