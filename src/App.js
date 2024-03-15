7c7f6d14b54e46c9dd54106063cf835fcdd1e326
Valerii <badday182@gmail.com>
2024-03-15 12:09:03 +0200

    Add Banner.jsx

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React, { useState, useRef, useEffect } from "react";
-import { Tooltip as ReactTooltip } from "react-tooltip";
 import { renderToString } from "react-dom/server";
-import { v4 as uuidv4 } from "uuid";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useDispatch } from "react-redux";
-
-import { BlockButton } from "./components/addBlockButton";
-import Button from "react-bootstrap/Button";
-
-import { ImagineOptions } from "./components/ImagineOptions";
-
-import { Editor, tinymce } from "@tinymce/tinymce-react";
+import { Editor } from "@tinymce/tinymce-react";
 import { PacientInfoPattern } from "./patternsText/pacientInfoPattern";
 
 import { PacientCard } from "./components/PacientCard.js";
 import { addTextFromEditor } from "./components/redux/slices/documentSliseReducer.js";
-import TooltipWithImage from "./components/TooltipWithImage.js";
 import { VscClearAll } from "react-icons/vsc";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Banner from "./components/Banner/Banner.jsx";
 const App = () => {
   const dispatch = useDispatch();
 
@@ -31,98 +23,23 @@ const App = () => {
   const docTex = useSelector((state) => state.creatingDocument.documentText);
 
   const editorRef = useRef();
-  const [isBannerVisible, setIsBannerVisible] = useState(true);
-  // tinymce.activeEditor.setContent("<p>Hello world!</p>");
-  // editorRef.current.setContent("<p>Hello world!</p>");
 
-  // const pacientInfo = pacientInfoPattern().props.children
   const pacientInfo = renderToString(PacientInfoPattern());
-  // console.log(pacientInfo);
-  // document.querySelector('#')
-  // EditorContent = editorRef.current.getContent()
+
   const editorContent = () => {
-    // const text = renderToString(editorRef.current.getContent());
     const content = editorRef.current.getContent(); //берет текст с эдитора
     dispatch(addTextFromEditor(content));
   };
   const scrollToBottom = () => {
-    // if (editorRef.current) {
-    //   const editorBody = editorRef.current.getBody();
-    //   editorBody.scrollTo(0, editorBody.scrollHeight);
-    // }
     const editorContainer = document.querySelector(".mce-content-body ");
     if (editorContainer) {
       editorContainer.scrollIntoView({ behavior: "smooth", block: "end" });
     }
   };
-  // let editorContent =  editorRef.current.getContent()
-  useEffect(() => {
-    // Прокрутка вниз при инициализации
-    scrollToBottom();
-  }, []);
-  const closeBanner = () => {
-    setIsBannerVisible(false);
-  };
 
   return (
-    // <div className=" m-auto conteinerWidht d-flex flex-row p-3 position-relative ">
     <div className="conteinerWidht d-flex justify-content-center flex-wrap p-3">
-      {/* <div className=" me-3 p-3 rounded-3 border pacientStore"> */}
-
-      <div className={`banner rounded-3 ${isBannerVisible ? "" : "hidden"}`}>
-        <div className="content">
-          <button id="closeBanner" onClick={closeBanner}>
-            &times;
-          </button>{" "}
-          <p>На каву ₴ розробнику</p>
-          <div className="logo privat-logo">
-            <img
-              style={{ backgroundColor: "white" }}
-              src="https://d2z9uwnt8eubh7.cloudfront.net/media/default/0001/19/ac94eecabd0d3d915ab3ba18b6c4de6f22ad7dfe.png"
-              alt="ПриватБанк лого"
-            />
-          </div>
-          <div className="logo mono-logo">
-            <img
-              src="https://asset.brandfetch.io/id-CBRc8NA/idEsOSs4jS.jpeg?updated=1674203441813"
-              alt="Монобанк лого"
-            />
-          </div>
-          {/* <div className="logo youtube">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a0/YouTube_social_red_circle_%282017%29.svg"
-              alt="Ютуб лого"
-            />
-          </div> */}
-            <p>------</p>
-          <a
-            className="mb-2 logo"
-            href="https://www.youtube.com/@RayaX.project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="m-0"
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a0/YouTube_social_red_circle_%282017%29.svg"
-              alt="Ютуб лого"
-            />
-          </a>
-          <a
-            className="m-0 logo telegram-logo"
-            href="https://t.me/Raya_X"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="m-0"
-              // dataTooltip="AAAAAAAAAAAA"
-              src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg"
-              alt="Телеграм лого"
-            />
-          </a>
-        </div>
-      </div>
-
+      <Banner />
       <div className="pacientBlock mb-4 mx-4">
         {patientState.map((option) => (
           <PacientCard
@@ -131,22 +48,7 @@ const App = () => {
             id={option.id}
           />
         ))}
-        {/* <button
-          type="button"
-          className="btn btn-outline-warning"
-          onClick={() => {
-            const content = editorRef.current.getContent();
 
-            const parser = new DOMParser();
-            const decodedHTML = parser.parseFromString(content, "text/html")
-              .body.textContent;
-            if (decodedHTML.includes("div")) {
-              dispatch(addTextFromEditor(decodedHTML));
-            }
-          }}
-        >
-          Convert
-        </button> */}
         <button
           type="button"
           className="btn btn-danger btn-sm"
@@ -167,16 +69,13 @@ const App = () => {
         <Editor
           apiKey="62kbbg7407jjlea01hu71w9axyixiyxitsr8wtho4lnck72p"
           onInit={(evt, editor) => (editorRef.current = editor)}
-          // initialValue=""
-          // initialValue={pacientInfo}
-
           initialValue={docTex}
           init={{
             selector: "#myTextarea",
             browser_spellcheck: true,
-            spellchecker_language: 'uk',
+            spellchecker_language: "uk",
             contextmenu: true,
-            language: 'uk',
+            language: "uk",
             height: 650,
             width: 700,
             content_css: "/src/tineContent.css",
@@ -221,7 +120,6 @@ const App = () => {
         />
       </>
       <Analytics />
-
     </div>
   );
 };
